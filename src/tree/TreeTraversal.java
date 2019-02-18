80e0f737d1bbb87e4cee10af373a4bf442393ebc
Bandi, Kushal <kushal.bandi@gmail.com>
2019-02-18 11:47:00 -0700

    Separating Node class in to individual public class in package
    Signed-off-by: Bandi, Kushal <kushal.bandi@gmail.com>

--- src/tree/TreeTraversal.java
+++ src/tree/TreeTraversal.java
@@ -1,16 +1,5 @@
 package tree;
 
-class Node {
-	int data;
-	Node left, right;
-	
-	public Node(int data){
-		this.data = data;
-		left = right = null;
-		
-	}
-}
-
 public class TreeTraversal {
 	
 	Node root;
